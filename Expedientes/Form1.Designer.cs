5e21861e96eecf44811d69c4f422289513d9bc1e
ATZIN NATANAEL PACHECO ARELLANO <Atzin.Pacheco@alumno.udg.mx>
2021-05-22 01:31:45 -0500

    Agregar archivos de proyecto.

---
+++ Expedientes/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Expedientes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.b = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.d = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textoabuscar = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.c = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Asap", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(244, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RESULTADO DE BUSQUEDA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Asap", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // a
+            // 
+            this.a.Font = new System.Drawing.Font("Asap", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a.Location = new System.Drawing.Point(250, 82);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(337, 25);
+            this.a.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Asap", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(139, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Edad";
+            // 
+            // b
+            // 
+            this.b.Font = new System.Drawing.Font("Asap", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.Location = new System.Drawing.Point(250, 151);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(111, 25);
+            this.b.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Asap", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(139, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Coronavirus";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Asap", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(139, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Antecedentes";
+            // 
+            // d
+            // 
+            this.d.Font = new System.Drawing.Font("Asap", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d.Location = new System.Drawing.Point(250, 278);
+            this.d.Multiline = true;
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(337, 133);
+            this.d.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "GUARDAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.buscarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // buscarToolStripMenuItem
+            // 
+            this.buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            this.buscarToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.buscarToolStripMenuItem.Text = "Buscar";
+            this.buscarToolStripMenuItem.Click += new System.EventHandler(this.buscarToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textoabuscar);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(469, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 142);
+            this.panel1.TabIndex = 11;
+            this.panel1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(198, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 25);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Asap", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 18);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Nombre";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textoabuscar
+            // 
+            this.textoabuscar.Font = new System.Drawing.Font("Asap", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textoabuscar.Location = new System.Drawing.Point(76, 46);
+            this.textoabuscar.Name = "textoabuscar";
+            this.textoabuscar.Size = new System.Drawing.Size(132, 25);
+            this.textoabuscar.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Asap", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(111, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Buscar";
+            // 
+            // c
+            // 
+            this.c.Font = new System.Drawing.Font("Asap", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.Location = new System.Drawing.Point(250, 214);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(111, 25);
+            this.c.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.d);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox d;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buscarToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textoabuscar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox c;
+    }
+}
+
---
+++ Expedientes/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expedientes
+{
+    public partial class Form1 : Form
+    {
+        string Path = @"C:\\Users\\Atz1NNN\\Desktop\\Expedientes";
+        ABB arbol = new ABB();
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            
+        }
+        public class Pacientes
+        {
+            public string name; 
+            public int edad;
+            public string coronavirus;
+            public string expediente;
+            public Pacientes()
+            {
+                name = "";
+                edad = 0;
+                coronavirus = "";
+                expediente = "";
+            }
+        }
+        
+            
+        
+        public class Nodo
+        {
+            public Nodo hi;
+            public Nodo hd;
+            public Pacientes valor;
+
+            public Nodo()
+            {
+                hi = null;
+                hd = null;
+                valor = new Pacientes();
+
+            }
+        }
+        public class ABB
+        {
+            public Nodo raiz;
+
+            public ABB()
+            {
+                raiz = null;
+            }
+            public void insertar(Pacientes valor)
+            {
+                if (raiz == null)
+                {
+                    raiz = new Nodo();
+                    raiz.valor = valor;
+                }
+                else
+                {
+                    Nodo nuevo = new Nodo();
+                    nuevo.valor = valor;
+                    nuevo.hi = null;
+                    nuevo.hd = null;
+                    Nodo anterior = null, recorrer;
+                    recorrer = raiz;
+                    while (recorrer != null)
+                    {
+                        anterior = recorrer;
+                        if (String.Compare(recorrer.valor.name, valor.name)< 0)
+                        {
+                            recorrer = recorrer.hd;
+
+                        }
+                        else
+                        {
+                            recorrer = recorrer.hi;
+                        }
+                    }
+                    if (String.Compare(valor.name, anterior.valor.name) > 0)
+                    {
+                        anterior.hd = nuevo;
+                    }
+                    else
+                    {
+                        anterior.hi = nuevo;
+                    }
+                }
+            }
+            public Pacientes Buscar(string valor)
+            {
+                if (raiz == null)
+                {
+                    MessageBox.Show("No hay nada");
+                    return new Pacientes();
+                }
+                else
+                {
+                   
+                    Nodo anterior = null, recorrer;
+                    recorrer = raiz;
+                    while (recorrer != null)
+                    {
+                        anterior = recorrer;
+                        if (String.Compare(recorrer.valor.name, valor) < 0)
+                        {
+                            recorrer = recorrer.hd;
+
+                        }
+                        else if  (String.Compare(recorrer.valor.name, valor) > 0)
+                        {
+                            recorrer = recorrer.hi;
+                        }
+                        else
+                        {
+                            return recorrer.valor;
+                        }
+
+                    }                 
+                }
+                MessageBox.Show("No hay nada");
+                return new Pacientes();
+            }
+            
+        }
+
+       
+        private void buscarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Pacientes chido = arbol.Buscar(textoabuscar.Text);         
+            a.Text = chido.name;
+            b.Text = chido.edad.ToString();
+            c.Text = chido.coronavirus;
+            d.Text = chido.expediente;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetFiles();
+        }
+        public void GetFiles()
+        {
+            
+            
+            string aux = "", nombre;
+            string[] archivos = Directory.GetFiles(Path); 
+            foreach(var archivo in archivos)
+            {
+                nombre = archivo.Substring(0, archivo.Length - 4);//para eliminar el ".txt"
+
+                aux = "";
+                for (int i = nombre.Length - 1; nombre[i] != '\\' && i >= 0; --i)
+                {
+                    aux += nombre[i];
+                    
+                }
+                nombre = "";
+                for (int i = aux.Length - 1; i >= 0; --i)
+                    nombre += aux[i];
+
+                Pacientes nom = new Pacientes();
+                nom.name = nombre;
+                arbol.insertar(nom);
+
+
+                string[] contenido = File.ReadAllLines(archivo);
+                int contador = 0;
+                foreach (string linea in contenido)
+                {
+                    if (contador == 0)
+                    {
+                        nom.edad = int.Parse(linea);
+                        contador++;
+                    }
+                    else if (contador == 1)
+                    {
+                        nom.coronavirus = linea;
+                        contador++;
+
+                    }
+                    else if (contador >= 2)
+                    {
+                        nom.expediente += linea;
+                        
+                    }
+
+                }
+               
+
+            }
+            
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pacientes chido = arbol.Buscar(a.Text);
+            chido.edad =  int.Parse(b.Text);
+            chido.coronavirus = c.Text;
+            chido.expediente = d.Text;      
+            File.WriteAllText("C:\\Users\\Atz1NNN\\Desktop\\Expedientes\\" + chido.name + ".txt", chido.edad + "\n" + chido.coronavirus + "\n" + chido.expediente + "\n");
+            a.Text = "";
+            b.Text = "";
+            c.Text = "";
+            d.Text = "";
+
+
+
+        }
+    }
+}
